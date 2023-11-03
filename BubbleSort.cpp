1b2d6f23cb07ddcc79074cb835514236cad368da
tanvirahmodrafi <87052044+tanvirahmodrafi@users.noreply.github.com>
2023-11-03 09:20:18 +0600

    added comments

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <unordered_set>
 using namespace std;
 
 
--- SelectionSort.cpp
+++ SelectionSort.cpp
@@ -16,20 +16,20 @@ int main()
 		cin >> arr[i];
 	}
 
-	for (int i = 0; i < n - 1; i++)
+	for (int i = 0; i < n - 1; i++)//{1,4,5,8} just taking 1 to 5;
 	{
-		for (int j = i + 1; j < n; j++)
+		for (int j = i + 1; j < n; j++)//{1,4,5,8} compareingwith 4 to 8
 		{
 			if (arr[i] >= arr[j])
 			{
 				temp = arr[j];
-				arr[j] = arr[i];
+				arr[j] = arr[i];//swaping with the bigger values
 				arr[i] = temp;
 			}
 			
 		}
 	}
-	cout<<"The sortedn array is: ";
+	cout<<"The sorted array is: ";
 	for (int i = 0; i < n; i++)
 	{
 		cout << arr[i] << ' ';
