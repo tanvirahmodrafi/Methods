53841ddd6f294457f1c2846e67dccd94a494006a
tanvirahmodrafi <87052044+tanvirahmodrafi@users.noreply.github.com>
2023-12-12 21:30:39 +0600

    updated

--- SelectionSort.cpp
+++ SelectionSort.cpp
@@ -16,19 +16,18 @@ int main()
 		cin >> arr[i];
 	}
 
-	for (int i = 0; i < n - 1; i++)//{1,4,5,8} just taking 1 to 5;
-	{
-		for (int j = i + 1; j < n; j++)//{1,4,5,8} compareing with 4 to 8
-		{
-			if (arr[i] >= arr[j])
-			{
-				temp = arr[j];
-				arr[j] = arr[i];//swaping with the bigger values
-				arr[i] = temp;
-			}
-			
-		}
-	}
+	for (int i = 0; i < n; i++)
+    {
+        int min = i;
+        for (int j = i+1; j < n; j++)
+        {
+            if (arr[min] > arr[j])
+            {
+                min = j;
+            }
+        }
+        swap(arr[i],arr[min]);
+    }
 	cout<<"The sorted array is: ";
 	for (int i = 0; i < n; i++)
 	{
