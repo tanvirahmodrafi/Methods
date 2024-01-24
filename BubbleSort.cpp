a8b1c9e7ff2898bdf5bf0e86bc8ff06820b2501d
tanvirahmodrafi <87052044+tanvirahmodrafi@users.noreply.github.com>
2024-01-24 12:23:03 +0600

    update

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -12,7 +12,6 @@ int main()
     {
         cin>>arr[i];
     }
-
     int temp = INT_MIN;
     for (int i = 0; i < n; i++)
     {
@@ -23,19 +22,14 @@ int main()
                 temp = arr[j];//saving the value.
                 arr[j] = arr[j+1];//swaping the value with bigger elements.
                 arr[j+1] = temp;
-
             }
-            
         }
-        
     }
     cout<<"Sorted array is: ";
     for (int i = 0; i < 5; i++)
     {
         cout<<arr[i]<<" ";
     }
-    
-    
-    
+
     return 0;
 }
--- SelectionSort.cpp
+++ SelectionSort.cpp
@@ -3,36 +3,35 @@
 
 using namespace std;
 
-int n,temp;
+int n, temp;
 
 int main()
 {
-	cout << "Enter the size of the array"<<'\n';
+	cout << "Enter the size of the array" << '\n';
 	cin >> n;
 	int arr[n];
-	cout << "Enter the elements of the array"<<'\n';
+	cout << "Enter the elements of the array" << '\n';
 	for (int i = 0; i < n; i++)
 	{
 		cin >> arr[i];
 	}
 
 	for (int i = 0; i < n; i++)
-    {
-        int min = i;
-        for (int j = i+1; j < n; j++)
-        {
-            if (arr[min] > arr[j])
-            {
-                min = j;
-            }
-        }
-        swap(arr[i],arr[min]);
-    }
-	cout<<"The sorted array is: ";
+	{
+		int min = i;
+		for (int j = i + 1; j < n; j++)
+		{
+			if (arr[min] > arr[j])
+			{
+				min = j;
+			}
+		}
+		swap(arr[i], arr[min]);
+	}
+	cout << "The sorted array is: ";
 	for (int i = 0; i < n; i++)
 	{
 		cout << arr[i] << ' ';
 	}
-
 	return 0;
 }
