897d1310cb30fc580a80a2c14cc2963d60067ba4
tanvirahmodrafi <87052044+tanvirahmodrafi@users.noreply.github.com>
2024-05-24 11:40:10 +0600

    update

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -1,55 +1,66 @@
 #include <iostream>
 using namespace std;
 
-void concuer(int arr[],int s,int mid,int e){
-    int *merg = new int[e-s+1];
-    int i = s, j= mid+1, k = 0;
-    while(i<=mid && j<= e){
-        if(arr[i] >= arr[j] ){
+void concuer(int arr[], int s, int mid, int e)
+{
+    int *merg = new int[e - s + 1];
+    int i = s, j = mid + 1, k = 0;
+    while (i <= mid && j <= e)
+    {
+        if (arr[i] >= arr[j])
+        {
             merg[k] = arr[j];
-            k++,j++;
-        }else{
-            merg[k] = arr[i];
-            k++,i++;
+            k++, j++;
+        }
+        else
+        {
+            merg[k] = arr[i];  //     1, 4, 6,             2, 3, 9        merge-> 1, 2, 3, 4, 6, 9
+            k++, i++;
         }
     }
-    while(i <= mid){
+    while (i <= mid)
+    {
         merg[k] = arr[i];
-        k++,i++;
+        k++, i++;
     }
-    while(j<= e){
+    while (j <= e)
+    {
         merg[k] = arr[j];
-        k++,j++;
+        k++, j++;
     }
-    for (int i = s,j = 0; i <= e; i++,j++){
-        arr[i] = merg[j];}
+    for (int i = s, j = 0; i <= e; i++, j++)
+    {
+        arr[i] = merg[j];
+    }
 }
 void divide(int arr[], int s, int e)
 {
-    if (e > s){
-        int mid = s + (e - s) / 2;
+    if (e > s)
+    {
+        int mid = s + (e - s) / 2;  
         divide(arr, s, mid);
         divide(arr, mid + 1, e);
-        concuer(arr,s,mid,e);
+        concuer(arr, s, mid, e);
     }
 }
+
 
 int main()
 {
     int n;
-    cout<<"Enter the number of array"<<endl;
+    cout << "Enter the number of array" << endl;
     cin >> n;
     int arr[n];
-    cout<<"Enter elements for the array"<<'\n';
+    cout << "Enter elements for the array" << '\n';
     for (int i = 0; i < n; i++)
     {
-        cin>>arr[i];
+        cin >> arr[i];
     }
-    divide(arr, 0, n-1);
-    cout<<"Your sorted array, using merge sort is "<<endl;
+    divide(arr, 0, n - 1);
+    cout << "Your sorted array, using merge sort is " << endl;
     for (int i = 0; i < n; i++)
     {
-        cout<<arr[i]<<' ';
+        cout << arr[i] << ' ';
     }
     return 0;
 }
--- SelectionSort.cpp
+++ SelectionSort.cpp
@@ -1,5 +1,4 @@
-#include <iostream>
-#include <cmath>
+#include <bits/stdc++.h>
 
 using namespace std;
 
@@ -35,3 +34,9 @@ int main()
 	}
 	return 0;
 }
+
+//      4, 6, 1, 3, 7
+
+//      1, 6, 4, 3, 7
+
+//      1, 3, 4, 6, 7
